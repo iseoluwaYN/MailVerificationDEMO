fc26840e49f5ed046d40c482d17496ee439c3203
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-14 03:35:46 +0100

    mail test

--- src/test/java/com/example/registrationDemo/mail/EmailServiceImplTest.java
+++ src/test/java/com/example/registrationDemo/mail/EmailServiceImplTest.java
@@ -1,4 +1,41 @@
+package com.example.registrationDemo.mail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmailServiceImplTest {
-  
+
+    @SpringBootTest
+    class MailGunEmailServiceImplTest {
+
+        @Autowired
+        @Qualifier("mailgun")
+        EmailService emailService;
+
+        @BeforeEach
+        void setUp() {
+        }
+
+        @Test
+        void sendEmailWithMailGunTest(){
+            //create message
+            Message mail = Message.builder()
+                    .from("iseoluwafasoyin@gmail.com")
+                    .to("ise@mailpoof.com")
+                    .subject("Test email")
+                    .body("This is the test body").build();
+
+            //call send method
+            MailResponse response = emailService.send(mail);
+
+            //verify the mail response
+            assertTrue(response.isSuccessful());
+        }
+    }
+
 }

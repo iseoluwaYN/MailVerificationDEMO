58e7b52eb7f661e4973f5c7d5b93996dfdfa832d
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-23 17:50:54 +0100

    sms otp test

--- src/test/java/com/example/registrationDemo/sms/SmsVerificationImplTest.java
+++ src/test/java/com/example/registrationDemo/sms/SmsVerificationImplTest.java
@@ -1,4 +1,32 @@
+package com.example.registrationDemo.sms;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class SmsVerificationImplTest {
-  
+
+    @Autowired
+    SmsVerificationImpl smsVerification;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testSmsOtp(){
+        SmsOtp sms = new SmsOtp();
+        sms.setTo("+2348183305446"); sms.setFrom("+19034033068");
+        sms.setBody(
+        "Iseoluwa sends her love to you <3! This was sent via code");
+        String sample = smsVerification.sample(sms);
+        System.out.println(sample);
+    }
 }
